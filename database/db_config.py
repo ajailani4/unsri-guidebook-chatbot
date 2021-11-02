3db54e1aa6e6719cd582d7d71cc7f0600eb3b839
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-11-02 17:55:37 +0700

    Create environment variables and configure app to deployed database in  Heroku

--- database/db_config.py
+++ database/db_config.py
@@ -1,17 +1,21 @@
 import psycopg2
 import asyncio
+import os
+from dotenv import load_dotenv
 
 class DbConfig:
+
   def __init__(self):
+    load_dotenv()
     super().__init__()
 
   def get_db(self):
     db = psycopg2.connect(
-        host = "127.0.0.1",
-        port = "5432",
-        database="unsri_guidebook",
-        user="postgres",
-        password="admin123"
+        host = os.environ.get("HOST"),
+        port = os.environ.get("PORT"),
+        database = os.environ.get("DATABASE"),
+        user = os.environ.get("USER"),
+        password = os.environ.get("PASSWORD")
       )
 
     return db
